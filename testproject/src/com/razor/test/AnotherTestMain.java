7e0a66215ef114d03d1e74e6aecb0b8f175e6bff
Andy Crutchlow <andyc@cogeco.ca>
2012-02-06 16:04:31 -0500

    Initial Commit testproject and testproject2

---
+++ testproject/src/com/razor/test/AnotherTestMain.java
@@ -0,0 +1,32 @@
+package com.razor.test;
+
+public class AnotherTestMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		int i = 0;
+		for (;i<10;i++){
+			System.out.println("i="+i);
+			System.out.println("Hello YYYY");
+			method1(i);
+		}
+		System.out.println("Hello WWWWW");
+		System.out.println("Hello CCCC");
+		System.out.println("Hello DDDDDD");
+	}
+
+	static void method1(int x) {
+		System.out.println("x="+x);
+	}
+	static void method2(int x) {
+		System.out.println("x="+x);
+	}
+
+	static void method3(int x) {
+		System.out.println("x="+x);
+	}
+
+}
---
+++ testproject/src/com/razor/test/TestMain.java
@@ -0,0 +1,14 @@
+package com.razor.test;
+
+public class TestMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Hello World2");
+		System.out.println("Hello World3");
+	}
+
+}
