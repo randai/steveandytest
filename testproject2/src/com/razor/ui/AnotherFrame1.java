e753b06dbfc3626f3fbd504b54e501ad3a799549
Andy Crutchlow <andycrutchlow@gmail.com>
2012-02-13 10:57:25 -0500

    some change

--- testproject2/src/com/razor/ui/AnotherFrame1.java
+++ testproject2/src/com/razor/ui/AnotherFrame1.java
@@ -5,7 +5,7 @@ public class AnotherFrame1 {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) {	
 		// TODO Auto-generated method stub
 //xxxxxxxxxxxxxxxxxxxxxxx
 	}
