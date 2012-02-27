89f897daec2b7e4828d0a5348d31e16319933ad8
Andy Crutchlow <andycrutchlow@gmail.com>
2012-02-27 13:50:38 -0500

    created testproject4 ..spring/maven

---
+++ testproject4/src/main/java/com/nts/main/testproject4/IBean1.java
@@ -0,0 +1,6 @@
+package com.nts.main.testproject4;
+
+public interface IBean1 {
+    String hello(String name);
+    void setVar1(String var1);
+}
---
+++ testproject4/src/test-integrate/java/com/nts/main/testproject4/util/ContextConfigurationTestRunner.java
@@ -0,0 +1,15 @@
+package com.nts.main.testproject4.util;
+
+import com.nts.main.testproject4.IBean1;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import static org.fest.assertions.Assertions.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:/META-INF/application-context-root.xml"})
+public class ContextConfigurationTestRunner {
+
+}
